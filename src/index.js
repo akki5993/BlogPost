aed9be4586e862c6016ff88c1418bd1588e0ddee
akki5993 <akshaypatel5991@gmail.com>
2024-08-06 09:44:23 +0530

    add folders structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-const app = express();
+
 const PORT = process.env.PORT || 8080;
+const app = express();
 
 app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.json({ msg: "Welcome" });
 });
 
 app.listen(PORT, (req, res) => {
-  console.log(`Server Running on PORT :- ${PORT}`);
+  console.log(`Server is Runnig On localhost:${PORT}`);
 });
